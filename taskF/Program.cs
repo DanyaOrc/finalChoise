b421dfe642420685be5e1811122eae1274728374
DanyaOrc <di@trillion.jewelry>
2023-04-10 13:58:57 +0300

    Add task and first method

---
+++ taskF/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    // Метод, который выводит элементы массива на консоль
+    static void PrintArray(string[] array)
+    {
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+        Console.WriteLine();
+    }
+}
